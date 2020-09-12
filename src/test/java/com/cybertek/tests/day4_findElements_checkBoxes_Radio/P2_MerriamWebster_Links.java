e3da9369e6d260b11ad48866628479af96a41d58
zainafzal-tek <zainafzal@gmail.com>
2020-09-12 17:01:09 -0400

    Day 04 Selenium class (CheckBoxes-RadioButtons)

---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkBoxes_Radio/P2_MerriamWebster_Links.java
@@ -0,0 +1,53 @@
+package com.cybertek.tests.day4_findElements_checkBoxes_Radio;
+
+import com.cybertek.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class P2_MerriamWebster_Links {
+
+    public static void main(String[] args) {
+
+        //TC #2: FINDELEMENTS
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //2. Go to https://www.merriam-webster.com/
+        driver.get("https://www.merriam-webster.com/");
+        //3. Print out the texts of all links
+        //Locating all of the web elements on the page and storing in a list of WebElement
+        List<WebElement> listOfLinks = driver.findElements(By.xpath("//body//a"));
+
+        for (WebElement eachLink : listOfLinks){
+            System.out.println(eachLink.getText());
+        }
+
+        //4. Print out how many link is missing text
+        int linksWithoutText = 0;
+
+        for (WebElement eachLink : listOfLinks){
+            if (eachLink.getText().equals("")){
+                linksWithoutText++;
+            }
+        }
+        System.out.println("Number of links missing text is = " + linksWithoutText);
+
+        //5. Print out how many link has text
+        int linksWithText = 0;
+
+        for (WebElement eachLink : listOfLinks){
+            if (!eachLink.getText().isEmpty()){
+                linksWithText++;
+            }
+        }
+        System.out.println("Number of links with text = " + linksWithText);
+
+        //6. Print out how many total link
+
+        System.out.println("Total number of links = " + (linksWithoutText + linksWithText));
+
+    }
+
+}
---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkBoxes_Radio/P3_CheckBox_Practice.java
@@ -0,0 +1,60 @@
+package com.cybertek.tests.day4_findElements_checkBoxes_Radio;
+
+import com.cybertek.tests.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class P3_CheckBox_Practice {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Practice: Cybertek Checkboxes
+        //1. Go to http://practice.cybertekschool.com/checkboxes
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/checkboxes");
+
+        //2. Confirm checkbox #1 is NOT selected by default
+        WebElement checkBox1 = driver.findElement(By.xpath("//form[@id = 'checkboxes']/input[1]"));
+
+        if (!checkBox1.isSelected()){
+            System.out.println("CheckBox 1 is not selected. Verification passed!");
+        } else {
+            System.out.println("CheckBox 1 is selected. Verification failed!");
+        }
+
+        //3. Confirm checkbox #2 is SELECTED by default.
+        WebElement checkBox2 = driver.findElement(By.xpath("//form[@id = 'checkboxes']/input[2]"));
+
+        if (checkBox2.isSelected()){
+            System.out.println("CheckBox 2 is selected. Verification passed!");
+        } else {
+            System.out.println("CheckBox 2 is not selected. Verification failed!");
+        }
+
+        //4. Click checkbox #1 to select it.
+        Thread.sleep(1000);
+        checkBox1.click();
+
+        //5. Click checkbox #2 to deselect it.
+        Thread.sleep(1000);
+        checkBox2.click();
+
+        //6. Confirm checkbox #1 is SELECTED.
+        if (checkBox1.isSelected()){
+            System.out.println("CheckBox 1 is selected. Verification passed!");
+        } else {
+            System.out.println("CheckBox 1 is not selected. Verification failed!");
+        }
+
+        //7. Confirm checkbox #2 is NOT selected.
+        if (!checkBox2.isSelected()){
+            System.out.println("CheckBox 2 is not selected. Verification passed!");
+        } else {
+            System.out.println("CheckBox 2 is selected. Verification failed!");
+        }
+
+
+    }
+
+}
