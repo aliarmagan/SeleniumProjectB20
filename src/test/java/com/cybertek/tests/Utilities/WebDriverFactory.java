d63f210b59bb8f6696b37b886ea4876e2700f3ee
mafzalch <mafzalch@gmail.com>
2020-09-14 21:23:46 -0400

    Update WebDriverFactory

--- src/test/java/com/cybertek/tests/Utilities/WebDriverFactory.java
+++ src/test/java/com/cybertek/tests/Utilities/WebDriverFactory.java
@@ -1,9 +1,15 @@
 package com.cybertek.tests.Utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WebDriverFactory {
 
@@ -21,6 +27,16 @@ public class WebDriverFactory {
             System.out.println("Given browser does not exist. Driver = null");
             return null;
         }
+
+    }
+
+    public void getScreenShot(WebDriver driver, String screenShotName) throws IOException {
+
+        //Calling getScreenShotAs method to create new img file:
+        File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+        //copying file to new location:
+        FileUtils.copyFile(srcFile, new File("/Users/mohammadafzal/Desktop/Zain School/" + screenShotName));
 
     }
 
