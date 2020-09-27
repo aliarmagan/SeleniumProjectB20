ed4a7ae9d6c6f4d505ae768cf8337101961efb8c
Zain_Afzal <zainafzal@gmail.com>
2020-09-27 14:28:08 -0400

    update SmartBearTests_WebTables

--- src/test/java/com/cybertek/tests/day7_webTables/WebTableTasks.java
+++ src/test/java/com/cybertek/tests/day7_webTables/WebTableTasks.java
@@ -30,15 +30,23 @@ public class WebTableTasks {
 
         WebElement susansDate = driver.findElement(By.xpath("//td[.='Susan McLaren']/following-sibling::td[3]"));
 
-        String expectedDate = "01/05/2020";
+        String expectedDate = "01/05/2010";
         String actualDate = susansDate.getText();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e){
+            System.out.println(e);
+        }
 
         Assert.assertTrue(actualDate.equals(expectedDate), "Actual date is not equal to expected date");
 
-        Assert.assertEquals(actualDate, expectedDate, "Actual date is not equal to expected date");
+        //Assert.assertEquals(actualDate, expectedDate, "Actual date is not equal to expected date");
 
         //Assert.fail("I AM FAILING THIS TASK ON PURPOSE");
         //TODO:FIX LOCATOR ASAP
+
+        driver.close();
 
     }
 
