1e897a29fc6315bbbc9ab4755611ea28069815a0
Zain_Afzal <zainafzal@gmail.com>
2020-09-30 18:58:17 -0400

    update iFramePractice

--- src/test/java/com/cybertek/tests/day9_windows_tabs/Alert3Practice.java
+++ src/test/java/com/cybertek/tests/day9_windows_tabs/Alert3Practice.java
@@ -1,2 +1,75 @@
-package com.cybertek.tests.day9_windows_tabs;public class Alert3Practice {
+package com.cybertek.tests.day9_windows_tabs;
+
+import com.cybertek.Utilities.BrowserUtils;
+import com.cybertek.Utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Alert3Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/javascript_alerts");
+    }
+
+    /*
+    TC #3: Information alert practice
+    1. Open browser
+    2. Go to website: http://practice.cybertekschool.com/javascript_alerts
+    3. Click to “Click for JS Prompt” button
+    4. Send “hello” text to alert
+    5. Click to OK button from the alert
+    6. Verify “You entered: hello” text is displayed.
+     */
+
+    @Test
+    public void p3_prompt_alert_practice(){
+
+        WebElement promptAlertButton = driver.findElement(By.xpath("//button[.= 'Click for JS Prompt']"));
+        promptAlertButton.click();
+
+        //Using alert class to handle alert:
+        Alert alert = driver.switchTo().alert();
+
+        //browserUtils class we created to store commonly used utility methods
+        BrowserUtils.wait(2);
+
+        //Sending keys:
+        alert.sendKeys("Something is happening");
+
+        //accepting the alert:
+        alert.accept();
+
+        //finding result text and verifying is is displayed:
+        WebElement resultText = driver.findElement(By.id("result"));
+        Assert.assertTrue(resultText.isDisplayed(), "result text is not displayed!");
+
+        //waiting for browser to catch up:
+        BrowserUtils.wait(1);
+
+        //verify result contains text input:
+        String actualText = resultText.getText();
+        Assert.assertEquals(actualText, resultText, "Result text is not displayed, Verification failed!");
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        BrowserUtils.wait(1);
+        driver.close();
+    }
+
 }
--- src/test/java/com/cybertek/tests/day9_windows_tabs/WindowHandlingPractices.java
+++ src/test/java/com/cybertek/tests/day9_windows_tabs/WindowHandlingPractices.java
@@ -1,2 +1,91 @@
-package com.cybertek.tests.day9_windows_tabs;public class WindowHandlingPractices {
+package com.cybertek.tests.day9_windows_tabs;
+
+import com.cybertek.Utilities.BrowserUtils;
+import com.cybertek.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class WindowHandlingPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void p3_handling_windows(){
+        driver.get("http://practice.cybertekschool.com/windows");
+
+        String actualTitle = driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+        Assert.assertTrue(actualTitle.contains("Practice"));
+
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        String titleAfterClick = driver.getTitle();
+        System.out.println("titleAfterClick = " + titleAfterClick);
+
+        String mainHandle = driver.getWindowHandle();
+        System.out.println("mainHandle = " + mainHandle);
+
+        //Set allows only unique values:
+        Set<String> windowHandles = driver.getWindowHandles();
+        // we loop through the set of window handles so that we can get the window handle of the other windows
+        // then using switchTo().window() method we switch to the other windows
+
+        for (String each : windowHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current Title = " + driver.getTitle());
+            System.out.println("Current window handle: " + driver.getWindowHandle());
+        }
+
+        //getting the current title(actual), and comparing with expected title:
+        Assert.assertTrue(driver.getTitle().equals("New Window"));
+
+        BrowserUtils.wait(1);
+        driver.close(); //driver.close will only close the current focused window/tab
+        //driver.quit(); this will close all instances adn will cause other tests to not be able to run:
+
+        //if i want to switch back to main menu to do some other things:
+        driver.switchTo().window(mainHandle);
+
+    }
+
+    @Test
+    public void p6_handling_more_than_two_windows(){
+        driver.get("https://amazon.com");
+
+        //This line is basically: We are downCasting our driver type to javascriptExecutor:
+        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+        Set<String> windowHandles= driver.getWindowHandles();
+
+        for (String each : windowHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current page title: " + driver.getTitle());
+
+            if (driver.getCurrentUrl().contains("etsy")){
+                Assert.assertTrue(driver.getTitle().contains("Etsy"));
+                break;
+            }
+        }
+
+    }
+
 }
