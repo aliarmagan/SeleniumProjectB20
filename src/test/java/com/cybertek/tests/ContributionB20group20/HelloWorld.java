7dd7d32e089b453ec34e7b60f9bb1f7167853663
Zain_Afzal <zainafzal@gmail.com>
2020-10-01 22:53:16 -0400

    update HelloWorld

--- src/test/java/com/cybertek/tests/ContributionB20group20/HelloWorld.java
+++ src/test/java/com/cybertek/tests/ContributionB20group20/HelloWorld.java
@@ -1,4 +1,11 @@
 package com.cybertek.tests.ContributionB20group20;
 
 public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+    }
+
 }
