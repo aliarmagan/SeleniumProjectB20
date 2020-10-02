d33991e6971f9d2ae4070386409bbf96314c7fc3
RGallardo <ragg86@gmail.com>
2020-10-01 23:03:32 -0400

    Added setUpMethod

--- src/test/java/com/cybertek/Utilities/BrowserUtility/WebDriverFactory.java
+++ src/test/java/com/cybertek/Utilities/BrowserUtility/WebDriverFactory.java
@@ -3,23 +3,47 @@ package com.cybertek.Utilities.BrowserUtility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class WebDriverFactory {
 
-    public static WebDriver getDriver(String browserType) {
+    public static WebDriver driver;
 
-        if (browserType.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
+    public void setUpMethod(){
+        //open a new browser
+        driver = WebDriverFactory.getDriver("Chrome");
+        //maximize the page
+        driver.manage().window().maximize();
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
-        } else if (browserType.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            return new FirefoxDriver();
+    public static WebDriver getDriver(String browserType){
+        browserType = browserType.toLowerCase();
 
-        } else {
-            System.out.println("Given browser does not exist. Driver = null");
-            return null;
+        switch (browserType) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                return new InternetExplorerDriver();
+            case "opera":
+                WebDriverManager.operadriver().setup();
+                return new OperaDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            default:
+                throw new RuntimeException("Invalid browser name");
         }
 
     }
