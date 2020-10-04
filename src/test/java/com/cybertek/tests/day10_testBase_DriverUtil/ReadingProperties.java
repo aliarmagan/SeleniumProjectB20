542ac418d8cb2f71ac6d5b772b07b82084b1b3d0
zainafzal-tek <zainafzal@gmail.com>
2020-10-04 14:16:55 -0400

    Update Day 10

---
+++ src/test/java/com/cybertek/tests/day10_testBase_DriverUtil/ReadingProperties.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day10_testBase_DriverUtil;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file(){
+
+        //Properties: Use properties class object
+        //FileInput Stream:
+        //We will load the file
+        //We will read from the file
+
+
+    }
+
+}
