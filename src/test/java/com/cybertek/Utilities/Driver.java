2b4c2a08848076627bddbe2043e002582ee714a0
Zain_Afzal <zainafzal@gmail.com>
2020-10-14 17:59:39 -0400

    update Driver

--- src/test/java/com/cybertek/Utilities/Driver.java
+++ src/test/java/com/cybertek/Utilities/Driver.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Driver {
 
     private Driver(){}
@@ -21,16 +23,22 @@ public class Driver {
                     case "chrome":
                         WebDriverManager.chromedriver().setup();
                         driver = new ChromeDriver();
+                        driver.manage().window().maximize();
+                        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                         break;
 
                     case "firefox":
                         WebDriverManager.chromedriver().setup();
                         driver = new FirefoxDriver();
+                        driver.manage().window().maximize();
+                        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                         break;
 
                     case "edge":
                         WebDriverManager.edgedriver().setup();
                         driver = new EdgeDriver();
+                        driver.manage().window().maximize();
+                        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                         break;
                 }
         }
