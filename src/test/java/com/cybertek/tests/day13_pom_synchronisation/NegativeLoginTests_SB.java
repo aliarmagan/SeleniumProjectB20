25125e7b14ded4c0eb47948f093ea29cbeb0017d
Zain_Afzal <zainafzal@gmail.com>
2020-10-14 20:59:04 -0400

    update Day 13

--- src/test/java/com/cybertek/tests/day13_pom_synchronisation/NegativeLoginTests_SB.java
+++ src/test/java/com/cybertek/tests/day13_pom_synchronisation/NegativeLoginTests_SB.java
@@ -8,6 +8,8 @@ import org.testng.annotations.Test;
 
 public class NegativeLoginTests_SB {
 
+    LoginPage loginPage;
+
     @Test(description = "entering incorrect username")
     public void negative_login_test(){
 
@@ -19,7 +21,7 @@ public class NegativeLoginTests_SB {
         //immediately initializes our diver and the object 'LoginPage'
 
         //#1 - we need to create the object of the class we want to use:
-        LoginPage loginPage = new LoginPage();
+        loginPage = new LoginPage();
 
         //#2 - Call the object to use the web elements:
         //entering incorrect username:
@@ -35,6 +37,21 @@ public class NegativeLoginTests_SB {
         //assert true that error message is displayed:
         Assert.assertTrue(loginPage.errorMessage.isDisplayed(), "Error message is not displayed. Verification failed!");
 
+    }
+
+    @Test (description = "using login method for negative test")
+    public void negative_login_test2(){
+
+        String url = ConfigurationReader.getProperty("smartBearUrl");
+        Driver.getDriver().get(url);
+
+        //re-instantiating object to avoid issues:
+        loginPage = new LoginPage();
+
+        String username = "asdfas";
+        String password = ConfigurationReader.getProperty("smartBear_Password");
+
+        loginPage.login(username, password);
 
     }
 
