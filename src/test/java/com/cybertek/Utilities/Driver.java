964fcb2201a45cd3df8e56cf37807e77b86dff21
zainafzal-tek <zainafzal@gmail.com>
2020-11-15 16:33:37 -0500

    update project

--- src/test/java/com/cybertek/Utilities/Driver.java
+++ src/test/java/com/cybertek/Utilities/Driver.java
@@ -3,8 +3,10 @@ package com.cybertek.Utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,16 +24,16 @@ public class Driver {
                 switch (browser){
                     case "chrome":
                         WebDriverManager.chromedriver().setup();
-                        driver = new ChromeDriver();
-                        driver.manage().window().maximize();
-                        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.setHeadless(true);
+                        driver = new ChromeDriver(chromeOptions);
                         break;
 
                     case "firefox":
                         WebDriverManager.chromedriver().setup();
-                        driver = new FirefoxDriver();
-                        driver.manage().window().maximize();
-                        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        firefoxOptions.setHeadless(true);
+                        driver = new FirefoxDriver(firefoxOptions);
                         break;
 
                     case "edge":
